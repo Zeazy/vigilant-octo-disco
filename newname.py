25c11caaf2c3defce4b2dfad26f08963f8e1dc17
zeazy <alfadelhamza@gmail.com>
2022-03-06 17:45:05 +0000

    new commit - destined for github

--- newname.py
+++ newname.py
@@ -1,9 +1,8 @@
 print('hello')
 print('Test')
 print('Testing restore')
-<<<<<<< HEAD
-print('test on new branches')
-=======
 print('New Branch stuff')
 print('Stuff i forgot to add to second branch commit')
->>>>>>> secondBranch
+
+for i in range(10):
+    print('New commit to github')
